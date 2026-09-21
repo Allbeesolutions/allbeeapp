952c46614c45548cf497be56c9d445df6ca70685
Hajihaz <iamhajihaz@gmail.com>
2026-09-22 03:08:41 +0530

    perf: narrow config and finance lock realtime events

--- src/AllbeeApp.jsx
+++ src/AllbeeApp.jsx
@@ -7202,8 +7202,8 @@ export default function App() {
     loadPeople(session.user);
     const ch = supabase.channel("allbee-people")
       .on("postgres_changes", { event: "*", schema: "public", table: "profiles" }, () => loadPeople(session.user))
-      .on("postgres_changes", { event: "*", schema: "public", table: "app_config" }, () => loadPeople(session.user))
-      .on("postgres_changes", { event: "*", schema: "public", table: "fin_locks" }, async () => setLocks(await fetchLocks()));
+      .on("postgres_changes", { event: "UPDATE", schema: "public", table: "app_config" }, () => loadPeople(session.user))
+      .on("postgres_changes", { event: "UPDATE", schema: "public", table: "fin_locks" }, async () => setLocks(await fetchLocks()));
     ch.subscribe();
     return () => { supabase.removeChannel(ch); };
   }, [session, loadPeople]);
